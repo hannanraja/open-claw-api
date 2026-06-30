26c8095e71caca6890b955101c71b5237deb6679
hannanraja <hannanraja1992@gmail.com>
2026-06-30 11:41:48 +0500

    update port and message

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 import { AppModule } from './app.module';
 import { setupSwagger } from './common/swagger/swagger.config';
 import { ResponseInterceptor } from './interceptor/response.interceptor';
+
+// Load .env file
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,7 +22,11 @@ async function bootstrap() {
   setupSwagger(app);
   app.useGlobalInterceptors(new ResponseInterceptor());
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+
+  console.log(`🚀 Server running on port ${port}`);
 }
 
 bootstrap();
